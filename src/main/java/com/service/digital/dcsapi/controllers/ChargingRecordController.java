7e9816b7ba7d6e8ff15bd1ebcd6ba45aa07c7fc6
Gabriel <gabrielmtvp@gmail.com>
2024-02-05 21:36:00 +0100

    Disabled cors

--- src/main/java/com/service/digital/dcsapi/controllers/ChargingRecordController.java
+++ src/main/java/com/service/digital/dcsapi/controllers/ChargingRecordController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/charge")
+@CrossOrigin(origins = "*")
 public class ChargingRecordController {
 
     @Autowired
