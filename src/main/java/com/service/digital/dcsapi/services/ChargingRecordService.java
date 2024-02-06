6b8d0c8d66e17d0695e7960ed1ecc7f086d46aa2
Gabriel <gabrielmtvp@gmail.com>
2024-02-06 21:15:59 +0100

    Removed spaces

--- src/main/java/com/service/digital/dcsapi/services/ChargingRecordService.java
+++ src/main/java/com/service/digital/dcsapi/services/ChargingRecordService.java
@@ -6,12 +6,9 @@ import com.service.digital.dcsapi.models.ChargingRecord;
 import java.util.List;
 
 public interface ChargingRecordService {
-
     ChargingRecord createCdr(ChargingRecord chargingRecord);
 
     ChargingRecord findLastCdrById();
 
     ChargingRecordsPage getAllChargingRecords(int page, int size, String sortProperty, String sortDirection, Long id);
-
-
 }
